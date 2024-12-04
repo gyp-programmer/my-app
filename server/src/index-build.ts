9918e7860663b7c2b3645752d909d36e757a2d12
grayson <grayson.gao@bvox.com>
2024-12-04 16:49:24 +0800

    feat: :art: 对接tiktok的退出接口，并且调整一些获取信息的接口请求头

--- server/src/index-build.ts
+++ server/src/index-build.ts
@@ -31,6 +31,53 @@ const regHost = /demo\.tiktoksa\.com/g;
 const mainHost = "https://demo.tiktoksa.com";
 const staticHost = "https://demo.tiktoksa.com/z-static";
 
+/** 退出 */
+router.get("/passport/web/logout/", (ctx: any) => {
+  const originUrl = url.parse(ctx.req.url);
+  const targetUrl = "https://www.tiktok.com";
+  const host = "www.tiktok.com";
+  const options = {
+    headers: {
+      ...ctx.request.header,
+      host,
+      "accept-encoding": "",
+    },
+  };
+
+  return new Promise((resolve, reject) => {
+    request(
+      targetUrl +
+        "/passport/web/logout/?" +
+        originUrl.query.replace(regHost, host),
+      options,
+      (error: any, response: any, body: any) => {
+        if (error) reject(error);
+        if (!error && response.statusCode === 200) {
+          // response.headers 中的
+          Object.keys(response.headers).forEach(key => {
+            ctx.set(key, response.headers[key]);
+          });
+          /** 设置cookies */
+          const cookies = response.headers["set-cookie"]
+            ? response.headers["set-cookie"].map((o: string) => {
+                return o.replace(/domain=tiktok.com;/g, cookieDomain.domain);
+              })
+            : [];
+          /** 为cookies中的每个key/value增加 SameSite=None; Secure */
+          ctx.set("set-cookie", cookies);
+          ctx.body = body;
+          resolve(true);
+        } else {
+          ctx.body = {
+            status: 502,
+            result: "forbidden",
+          };
+          resolve(true);
+        }
+      },
+    );
+  });
+});
 /** 关注 */
 router.post("/api/commit/follow/user/", (ctx: any) => {
   const originUrl = url.parse(ctx.req.url);
@@ -448,8 +495,8 @@ router.get("/tiktok/v1/kids/aweme/favorite/", (ctx: any) => {
     headers: {
       ...ctx.request.header,
       host,
-      origin: ctx.request.header["origin"].replace(regHost, host),
       referer: ctx.request.header["referer"].replace(regHost, host),
+      "accept-encoding": "",
     },
   };
 
@@ -2006,6 +2053,9 @@ router.get("/", getPage);
 router.get("/login", getPage);
 router.get("/signup", getPage);
 router.get("/foryou", getPage);
+router.get(/@[^?]+\\?lang=/, getPage);
+router.get("/logout", getPage);
+router.get("/setting", getPage);
 
 router.post("/tea/service/2/abtest_config/", (ctx: any) => {
   const teaUrl = "https://mcs-sg.tiktokv.com";
@@ -2397,11 +2447,13 @@ router.get("/node-webapp/api/common-app-context", (ctx: any) => {
   const originUrl = url.parse(ctx.req.url);
   const qs = querystring.parse(originUrl.query);
   const targetUrl = qs["lang"];
-
+  const host = "www.tiktok.com";
   const options = {
     headers: {
-      "user-agent":
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
+      ...ctx.request.header,
+      referer: ctx.request.header["referer"].replace(regHost, host),
+      host,
+      "accept-encoding": "",
     },
   };
   // 2.代发请求
@@ -2414,6 +2466,10 @@ router.get("/node-webapp/api/common-app-context", (ctx: any) => {
       (error: any, response: any, body: any) => {
         if (error) reject(error);
         if (!error && response.statusCode === 200) {
+          // response.headers 中的
+          Object.keys(response.headers).forEach(key => {
+            ctx.set(key, response.headers[key]);
+          });
           ctx.body = body;
           resolve(true);
         } else {
@@ -2433,12 +2489,13 @@ router.get("/node-webapp/api/biz-context", (ctx: any) => {
   const qs = querystring.parse(originUrl.query);
   const lang = qs["lang"];
   const app_name = qs["app_name"];
-
+  const host = "www.tiktok.com";
   const options = {
     headers: {
-      "user-agent":
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 16_6 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.6 Mobile/15E148 Safari/604.1",
-      //cookie: `_ttp=2Tdii7MlaZSEtTL8RC7ZqRWBBuM; tt_csrf_token=vbZnItDW-w9gedHbCSz8Fpw51jE4UjS3kbZ4; tiktok_webapp_theme=light; odin_tt=146e34dda42fac3ca4b14ed90a87be147a1f4267445fa42e34669a468240015f7376a8ac49453745959f0ba0404887490e58be6dbbaf7879506df313478457b4d2a4200ab687dae7e5d1ab2ea12cd7a5; delay_guest_mode_vid=3; s_v_web_id=verify_lzod3hx3_vkF2Nb7z_kGCE_4Shp_8Nw1_XyqaMpV0Hmji; ak_bmsc=834FE9A11D330EBAE6B055BE77B1CB69~000000000000000000000000000000~YAAQz1A7F+xBJieTAQAAW2qtOBkzx8FmnWdUSkv7mPZLagwGdqBZHkRiSwvShO+dz39gNSjfpQm8t+YwIlFF2u0KuFiyW4PyWScH0l6i9tPMF55puDcJWZvvLXMjTArB4tyyWUuG5IwXo9uUTFA8yNj0XOsxbPvnmy0aj3rVj3W+MpUQz93WP59acBtLAT489z2Hp1P+u5zApmc+9ubW4Bwusn+KQFUtoED5ymV7Vy3efQYTs5G71YwJAkjxFEcls9JFuLZ6iKfUHYPA/JZz5y8uMisWGp1TMwlcaX+9qvaP5nMl6INCNThUfbJYU6kaBCGH+VIXgCDD2im3cJioeL1f6MQQMiojGhoeXG/0ENFjRN/FqQyyoUh5XrxQ+DhwZ3wtLAVtQqkSG1Q=; tt_chain_token=nNKpBE0UTq/Nn4ZtqUnLWg==; bm_sv=97E2EC07BA4813B98E9512F72EBCBF40~YAAQz1A7F3BEJieTAQAAtsKuOBkif3Fq9gIfYTHbQ6wR6HoJrOKyjPNgUi0SiUR9xmrPxfhC1VML8+lruBWhbnyKb/sNsGGAkY+GtuVODKYOHvjV/74GZ37r12tz/Eg1586/UhW75DpCjM8/Z56xT5UbZ+cW4Sh/hfyLpGqdfijwYZbX0u6Rw9qCvbWWPaLdg9K42iH46Kxy3hbtLFxCmDWxqpn89Rr9P0ekUJKopsIeZcFyy8ePLETyL3xLfEJW~1; tiktok_webapp_theme_source=auto; ttwid=1%7CitwetinyOztdiIrMBwn1k-3Y6aOp9HBzd6TiYZ4tpKQ%7C1731822799%7Cb583630f99443e6df9fe9fb45dda965c0a262302a5d7aafb251684a08ad9f0b6; perf_feed_cache={%22expireTimestamp%22:1731992400000%2C%22itemIds%22:[%227428748283171949866%22%2C%227419234414162480391%22%2C%227421225623563095329%22]}; msToken=EL2Ncg69yxMtsQpbm5BTdz3hXEnZDaUlXFXVNuRiT8WDxhOiFoAwTXi1XjpC4QTisFh4EQ7i6Ed7IsUUTvoBFiGmk5CZuj5QyLiepHZ4L4XWxIacNyvVtPwQbKOidjuScRDF1mtXwei48Uui5ptuAEM5; msToken=EL2Ncg69yxMtsQpbm5BTdz3hXEnZDaUlXFXVNuRiT8WDxhOiFoAwTXi1XjpC4QTisFh4EQ7i6Ed7IsUUTvoBFiGmk5CZuj5QyLiepHZ4L4XWxIacNyvVtPwQbKOidjuScRDF1mtXwei48Uui5ptuAEM5`,
+      ...ctx.request.header,
+      referer: ctx.request.header["referer"].replace(regHost, host),
+      host,
+      "accept-encoding": "",
     },
   };
   // 2.代发请求
@@ -2450,6 +2507,10 @@ router.get("/node-webapp/api/biz-context", (ctx: any) => {
       (error: any, response: any, body: any) => {
         if (error) reject(error);
         if (!error && response.statusCode === 200) {
+          // response.headers 中的
+          Object.keys(response.headers).forEach(key => {
+            ctx.set(key, response.headers[key]);
+          });
           ctx.body = body;
           resolve(true);
         } else {
